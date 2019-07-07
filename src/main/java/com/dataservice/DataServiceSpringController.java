94487c64a58d308f40db6fe73b3b41571f06d6f9
Richard Whitney <rb1whitney@msn.com>
2019-07-07 15:45:12 +0000

    Updating version to 1.5

--- src/main/java/com/dataservice/DataServiceSpringController.java
+++ src/main/java/com/dataservice/DataServiceSpringController.java
@@ -4,13 +4,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /* Spring Auto Boot Class */
+@SpringBootApplication
+@Configuration
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "com.dataservice")
+@EnableJpaRepositories("com.dataservice.repository")
 public class DataServiceSpringController extends SpringBootServletInitializer {
 
     private static final Logger logger = LoggerFactory.getLogger(DataServiceSpringController.class);
