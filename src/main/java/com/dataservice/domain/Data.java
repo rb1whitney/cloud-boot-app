080fc1d2459f4d5c0aadccb6de83f0804b8f3b2e
Richard Whitney <rb1whitney@msn.com>
2026-04-08 21:39:14 -0600

    refactor(domain): Use Lombok for Data entity

--- src/main/java/com/dataservice/domain/Data.java
+++ src/main/java/com/dataservice/domain/Data.java
@@ -3,6 +3,9 @@ package com.dataservice.domain;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.xml.bind.annotation.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /* Domain Transaction Object Class */
 
@@ -10,6 +13,9 @@ import jakarta.xml.bind.annotation.*;
 @Table(name = "data")
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Data {
 
     @Id
@@ -22,47 +28,4 @@ public class Data {
 
     @Column()
     private String description;
-
-    public Data() {
-    }
-
-    public Data(long id, String name, String description) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    // Returns a basic construct for logging
-    @java.lang.Override
-    public java.lang.String toString() {
-        return "Data{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                '}';
-    }
 }
