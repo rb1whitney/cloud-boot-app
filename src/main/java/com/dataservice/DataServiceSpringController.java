8905efd672e73d9cf3a45e7cb1e8942de0180e09
Richard Whitney <rb1whitney@msn.com>
2026-04-08 21:43:12 -0600

    refactor(controller): Modernize VersionController and main Application class

--- src/main/java/com/dataservice/DataServiceSpringController.java
+++ src/main/java/com/dataservice/DataServiceSpringController.java
@@ -1,25 +1,17 @@
 package com.dataservice;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /* Spring Auto Boot Class */
 @SpringBootApplication
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan(basePackages = "com.dataservice")
 @EnableJpaRepositories("com.dataservice.repository")
+@Slf4j
 public class DataServiceSpringController extends SpringBootServletInitializer {
-
-    private static final Logger logger = LoggerFactory.getLogger(DataServiceSpringController.class);
 
 	public static void main(String[] args) {
         SpringApplication.run(DataServiceSpringController.class, args);
@@ -27,7 +19,7 @@ public class DataServiceSpringController extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        logger.info("Configuring application with Spring Boot");
+        log.info("Configuring application with Spring Boot");
         return application.sources(DataServiceSpringController.class);
     }
 }
--- src/main/java/com/dataservice/domain/Data.java
+++ src/main/java/com/dataservice/domain/Data.java
@@ -2,20 +2,11 @@ package com.dataservice.domain;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.xml.bind.annotation.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 /* Domain Transaction Object Class */
 
 @Entity
 @Table(name = "data")
-@XmlRootElement
-@XmlAccessorType(XmlAccessType.FIELD)
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Data {
 
     @Id
@@ -28,4 +19,46 @@ public class Data {
 
     @Column()
     private String description;
+
+    public Data() {
+    }
+
+    public Data(long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Data{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
