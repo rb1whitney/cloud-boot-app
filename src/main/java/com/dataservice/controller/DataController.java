8354a5278d2c566e2b4ba7c58c3b3cbd5092e346
Richard Whitney <rb1whitney@msn.com>
2026-04-08 21:40:51 -0600

    refactor(controller): Modernize DataController with Spring MVC annotations and Lombok

--- src/main/java/com/dataservice/controller/DataController.java
+++ src/main/java/com/dataservice/controller/DataController.java
@@ -3,8 +3,7 @@ package com.dataservice.controller;
 import com.dataservice.domain.Data;
 import com.dataservice.exception.ResourceNotFoundException;
 import com.dataservice.service.DataService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -18,9 +17,9 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/api/v1/data")
+@Slf4j
 public class DataController {
 
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private static final String DEFAULT_PAGE_SIZE = "10";
     private static final String DEFAULT_PAGE_NUM = "0";
 
@@ -29,38 +28,31 @@ public class DataController {
 
     // This function creates data if it is not there. If it is there, it will overwrite it. Location will be returned
     // back to user.
-    @RequestMapping(value = "",
-            method = RequestMethod.POST,
+    @PostMapping(value = "",
             consumes = {"application/json", "application/xml"},
             produces = {"application/json", "application/xml"})
     @ResponseStatus(HttpStatus.CREATED)
     public void createData(@RequestBody @Valid Data data, HttpServletRequest request, HttpServletResponse response) {
         Data createdData = this.dataService.createData(data);
-        logger.debug("Created following data: " + createdData);
+        log.debug("Created following data: {}", createdData);
         response.setHeader("Location", request.getRequestURL().append("/").append(createdData.getId()).toString());
     }
 
     // Gets all data that is available to the Data Service
-    @RequestMapping(value = "",
-            method = RequestMethod.GET,
+    @GetMapping(value = "",
             produces = {"application/json", "application/xml"})
     @ResponseStatus(HttpStatus.OK)
-    public
-    @ResponseBody
-    Page<Data> getAllData(@RequestParam(value = "page", required = true, defaultValue = DEFAULT_PAGE_NUM) Integer page,
-                          @RequestParam(value = "size", required = true, defaultValue = DEFAULT_PAGE_SIZE) Integer size,
-                          HttpServletRequest request, HttpServletResponse response) {
+    public @ResponseBody Page<Data> getAllData(@RequestParam(value = "page", defaultValue = DEFAULT_PAGE_NUM) Integer page,
+                                               @RequestParam(value = "size", defaultValue = DEFAULT_PAGE_SIZE) Integer size,
+                                               HttpServletRequest request, HttpServletResponse response) {
         return this.dataService.getAllData(page, size);
     }
 
     // Gets data via the resource id
-    @RequestMapping(value = "/{id}",
-            method = RequestMethod.GET,
+    @GetMapping(value = "/{id}",
             produces = {"application/json", "application/xml"})
     @ResponseStatus(HttpStatus.OK)
-    public
-    @ResponseBody
-    Data getData(@PathVariable("id") Long id, HttpServletRequest request, HttpServletResponse response)
+    public @ResponseBody Data getData(@PathVariable("id") Long id, HttpServletRequest request, HttpServletResponse response)
             throws ResourceNotFoundException {
         Data data = this.dataService.getData(id);
         checkResourceFound(data);
@@ -68,8 +60,7 @@ public class DataController {
     }
 
     // Updates data via resource id. Note that id is required in json or otherwise resource will not be found.
-    @RequestMapping(value = "/{id}",
-            method = RequestMethod.PUT,
+    @PutMapping(value = "/{id}",
             consumes = {"application/json", "application/xml"},
             produces = {"application/json", "application/xml"})
     @ResponseStatus(HttpStatus.NO_CONTENT)
@@ -78,19 +69,18 @@ public class DataController {
         checkResourceFound(this.dataService.getData(id));
         if (id != data.getId()) throw new ResourceNotFoundException();
         this.dataService.updateData(data);
-        logger.debug("Updated the following data: " + data);
+        log.debug("Updated the following data: {}", data);
     }
 
     // Deletes data via resource id
-    @RequestMapping(value = "/{id}",
-            method = RequestMethod.DELETE,
+    @DeleteMapping(value = "/{id}",
             produces = {"application/json", "application/xml"})
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteData(@PathVariable("id") Long id, HttpServletRequest request,
                            HttpServletResponse response) throws ResourceNotFoundException {
         checkResourceFound(this.dataService.getData(id));
         this.dataService.deleteData(id);
-        logger.debug("Deleted the following data associated with id: " + id);
+        log.debug("Deleted the following data associated with id: {}", id);
     }
 
     // Checks if resource was found from Data Service. Otherwise, will return basic exception
